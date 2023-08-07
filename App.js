51f0d970002e6810fa38ee76d294f2ce8f471f3c
jaden <asadsaifi90@gmail.com>
2023-08-07 16:45:34 -0500

    modified:   App.js 	modified:   package-lock.json 	modified:   package.json 	new file:   src/screens/HomeScreen.jsx 	modified:   tailwind.config.js

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./src/screens/HomeScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-green-500">
-        This shows that NativeWind is setup properly!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Welcome to SmartFood" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
