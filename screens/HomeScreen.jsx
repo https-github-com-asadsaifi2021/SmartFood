9377aba80cb7a2b11082790fb95471169b96b857
Aashish <takoaashish01@gmail.com>
2023-08-10 19:34:07 -0500

    Initialized sqllite  database.

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,31 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
   TouchableOpacity,
   TouchableWithoutFeedback,
   Keyboard,
+  Modal,
 } from "react-native";
-import { FontAwesome } from "@expo/vector-icons";
+import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 import { globalStyles } from "../styles/GlobalStyles";
 import AddItemForm from "./AddItemForm";
 
 const HomeScreen = () => {
+  const [addItemFormModal, setaddItemFormModal] = useState(false);
+
   return (
-    <TouchableWithoutFeedback
-      onPress={() => {
-        Keyboard.dismiss();
-      }}
-    >
-      <View className="flex-1 justify-center items-center">
-        <View>
-          <AddItemForm />
-        </View>
-        <TouchableOpacity style={globalStyles.addButtonContainer}>
-          <FontAwesome name="plus" style={globalStyles.addButtonIcon} />
-        </TouchableOpacity>
-      </View>
-    </TouchableWithoutFeedback>
+    <View style={globalStyles.container}>
+      <Modal visible={addItemFormModal} animationType="slide">
+        <TouchableWithoutFeedback
+          onPress={() => {
+            Keyboard.dismiss();
+          }}
+        >
+          <View style={globalStyles.container}>
+            <MaterialIcons
+              name="close"
+              style={globalStyles.modalClose}
+              size={24}
+              onPress={() => setaddItemFormModal(false)}
+            />
+            <AddItemForm />
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+      <TouchableOpacity
+        style={globalStyles.addButtonContainer}
+        onPress={() => setaddItemFormModal(true)}
+      >
+        <FontAwesome name="plus" style={globalStyles.addButtonIcon} />
+      </TouchableOpacity>
+    </View>
   );
 };
 
