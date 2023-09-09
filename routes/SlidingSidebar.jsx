bff4c65b292240cdc00902d89a8ea9fa85f568d4
Aashish <takoaashish01@gmail.com>
2023-09-08 21:35:23 -0500

    Modified: Drawer nav css

--- routes/SlidingSidebar.jsx
+++ routes/SlidingSidebar.jsx
@@ -2,14 +2,40 @@ import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "../screens/HomeScreen";
 import SettingScreen from "../screens/SettingScreen";
+import { FontAwesome } from "@expo/vector-icons";
 
 const Drawer = createDrawerNavigator();
 
 const SlidingSidebar = () => {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="HomeScreen" component={HomeScreen} />
-      <Drawer.Screen name="SettingScreen" component={SettingScreen} />
+    <Drawer.Navigator
+      screenOptions={{
+        drawerActiveTintColor: "blue",
+        drawerInactiveTintColor: "gray",
+        drawerItemStyle: { marginVertical: 2 },
+        drawerLabelStyle: { fontSize: 16 },
+      }}
+    >
+      <Drawer.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          drawerLabel: "Home",
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="SettingScreen"
+        component={SettingScreen}
+        options={{
+          drawerLabel: "Settings",
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome name="cog" color={color} size={size} />
+          ),
+        }}
+      />
       {/* Add more drawer items as needed */}
     </Drawer.Navigator>
   );
